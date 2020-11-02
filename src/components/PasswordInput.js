68d462dd124234da302e41feeec243a31ab042fe
Matthias Heisterkamp <hello@mhstrkmp.dev>
2020-11-02 16:12:51 +0100

    :sparkles: Add variable background color for password input based on length

--- src/components/PasswordInput.js
+++ src/components/PasswordInput.js
@@ -10,9 +10,13 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components/macro';
 
+function getHue(passwordLength = 0) {
+  return Math.min(passwordLength * 10, 100);
+}
 const Input = styled.input`
-  background: red;
+  background: hsl(${(props) => getHue(props.value.length)}, 60%, 60%);
 `;
+
 const PasswordInput = ({ value, onChange }) => {
   const [password, setPassword] = useState(value);
   useEffect(() => {
